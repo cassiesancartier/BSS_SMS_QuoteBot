b902025dd5981f2af042874433abb4bae4f76529
cassiesancartier <cassie.sancartier@gmail.com>
2013-08-28 21:28:09 -0400

    twilio update

--- local_settings.py
+++ local_settings.py
@@ -13,7 +13,7 @@ BSS_SPAM_ID = "+19789562878"
 
 # Begin Heroku configuration - configured through environment variables.
 import os
-ACCOUNT_SID = os.environ['ACba92b413588da3fb9e475a5344e0e3ce']
-AUTH_TOKEN = os.environ['183f87a88bf7230e8192ec1b54e5b68b']
-BSSSPAM_APP_SID = os.environ['APc36deb3789ab04c3918e72299365ca6d']
-BSS_SPAM_ID = os.environ['+19789562878']
+ACCOUNT_SID = os.environ['']
+AUTH_TOKEN = os.environ['']
+BSSSPAM_APP_SID = os.environ['']
+BSS_SPAM_ID = os.environ['']
