82822add7af2300f7fc5bcbf6f74ddad18782b8a
cassiesancartier <cassie.sancartier@gmail.com>
2013-08-28 21:18:11 -0400

    twilio update

--- local_settings.py
+++ local_settings.py
@@ -4,16 +4,16 @@ Configuration Settings
 Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 '''
 
-# Uncommet to configure in file.
-ACCOUNT_SID = "AC802d88dd13f2e3f8d8bc3f9b95eb9f06" 
-AUTH_TOKEN = "a610e5b81d025523706a2bbc381bf5ac"
-BSSSPAM_APP_SID = "AP69a2d4c9cdceee480dfa9e9230c586c5"
-BSS_SPAM_ID = "+17032913341"
+# Uncomment to configure in file.
+ACCOUNT_SID = "ACba92b413588da3fb9e475a5344e0e3ce" 
+AUTH_TOKEN = "183f87a88bf7230e8192ec1b54e5b68b"
+BSSSPAM_APP_SID = "APc36deb3789ab04c3918e72299365ca6d"
+BSS_SPAM_ID = "+19789562878"
 
 
 # Begin Heroku configuration - configured through environment variables.
 import os
-ACCOUNT_SID = os.environ['ACCOUNT_SID']
-AUTH_TOKEN = os.environ['AUTH_TOKEN']
-BSSSPAM_APP_SID = os.environ['BSSSPAM_APP_SID']
-BSS_SPAM_ID = os.environ['BSS_SPAM_ID']
+ACCOUNT_SID = os.environ['ACba92b413588da3fb9e475a5344e0e3ce']
+AUTH_TOKEN = os.environ['183f87a88bf7230e8192ec1b54e5b68b']
+BSSSPAM_APP_SID = os.environ['APc36deb3789ab04c3918e72299365ca6d']
+BSS_SPAM_ID = os.environ['+19789562878']
