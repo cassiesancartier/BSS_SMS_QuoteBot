8f9cfc8f9e19ff462a4997acd0f113664284d984
cassiesancartier <cassie.sancartier@gmail.com>
2013-08-28 21:26:17 -0400

    twilio update

--- local_settings.py
+++ local_settings.py
@@ -4,7 +4,7 @@ Configuration Settings
 Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 '''
 
-# Uncomment to configure in file.
+Uncomment to configure in file.
 ACCOUNT_SID = "ACba92b413588da3fb9e475a5344e0e3ce" 
 AUTH_TOKEN = "183f87a88bf7230e8192ec1b54e5b68b"
 BSSSPAM_APP_SID = "APc36deb3789ab04c3918e72299365ca6d"
